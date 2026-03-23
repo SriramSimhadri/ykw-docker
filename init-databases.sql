c1d6aff0f8f03a02b4d6ea6473f4fc17e0bec3a9
Sriram <Sriram>
2026-03-23 07:06:17 +0400

    Docker and DB scripts

---
+++ init-databases.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE user_db;
+CREATE DATABASE profile_db;
+CREATE DATABASE article_db;
+CREATE DATABASE article_likes_db;
+CREATE DATABASE article_comments_db;
+CREATE DATABASE follows_db;
